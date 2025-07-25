1bf141972149abf4f5156c219e001c596ac059c8
8VINO <viniciussouto1008@gmail.com>
2025-07-24 21:15:18 -0300

    Implementando Multa

--- src/Classes/Multa.java
+++ src/Classes/Multa.java
@@ -1,4 +1,22 @@
 package Classes;
 
 public class Multa {
+    private static int proximoId = 1;
+    private int idMulta;
+    private String cpfUsuario;
+    private int tomboLivro;
+    private double valor;
+    private boolean pago;
+
+    public Multa(String cpfUsuario,int tomboLivro,double valor, boolean pago){
+        this.idMulta = proximoId++;
+        this.cpfUsuario=cpfUsuario;
+        this.tomboLivro=tomboLivro;
+        this.valor=valor;
+        this.pago=pago;
+    }
+
+    public void setPago(){
+        this.pago=true;
+    }
 }
